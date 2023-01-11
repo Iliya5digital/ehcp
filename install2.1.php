96eedbd02b60915f3d2768efd91dd51be9b167ce
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-01-11 15:29:08 +0300

    Add files via upload

---
+++ install2.1.php
@@ -0,0 +1,15 @@
+<?php
+	$ehcpinstalldir='/var/www/new/ehcp';
+	$ip='192.168.1.3';
+	$user_name='bv';
+	$user_email='bvidinli@gmail.com';
+	$ehcpmysqlpass='';
+	$rootpass='';
+	$newrootpass='';
+	$ehcpadminpass='';
+	$hostname='bilg';
+	$lang='en';
+	$installextrasoftware='';
+
+
+	?>
